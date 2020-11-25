03b6c593a97c576dd1e5b6933a2606c2280a2513
Sara Cave <saracaveg@gmail.com>
2020-11-25 23:19:01 +1000

    Updates

---
+++ precache-manifest.bfd0ee84ac57b0d7e3db085081740c8a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef49d217ef580621a992b02939bd04c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfab0cf5d2e2c9cd299a",
+    "url": "/static/css/2.366b4564.chunk.css"
+  },
+  {
+    "revision": "0b88bb15416b085de0aa",
+    "url": "/static/css/main.1b27a777.chunk.css"
+  },
+  {
+    "revision": "dfab0cf5d2e2c9cd299a",
+    "url": "/static/js/2.7118cd6f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7118cd6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b88bb15416b085de0aa",
+    "url": "/static/js/main.ae97849d.chunk.js"
+  },
+  {
+    "revision": "a96b59cf9e48e15f994f",
+    "url": "/static/js/runtime-main.1e5d5210.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
